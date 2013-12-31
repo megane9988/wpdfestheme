e8a3485aeefdcdea11b919b30665d34f60a2f1de
megane9988 <info@m-g-n.me>
2014-01-01 05:57:20 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,19 @@
+		<div class="row">
+			<div class="large-6 columns">
+				<?php dynamic_sidebar('footer01'); ?>
+			</div>
+			<div class="large-6 columns">
+				<?php dynamic_sidebar('footer02'); ?>
+		</div>
+		<div class="row">
+			<div class="large-12 columns text-center">
+				<hr>
+				<p>copyright</p>
+				<p>Photo by <a href="http://www.flickr.com/photos/pawel_pacholec/">Pawel Pacholec</a> and <a href="http://www.flickr.com/photos/gianni_cumbo/">Gianni Cumbo</a></p>
+				</div>
+			</div>
+		</div>
+		<a id="pageTop" href="#top">▲TOP</a>
+		<?php wp_footer(); ?>
+	</body>
+</html>
